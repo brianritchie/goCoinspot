809d6094a04e836536d9c93864cdf4b1a3413f87
Brian Ritchie <sayhello@brianritchie.me>
2025-01-07 11:13:48 +1100

    Updated models with relevant structs and updated README with LICENSE details

--- README.md
+++ README.md
@@ -73,4 +73,4 @@ OUTPUT_DIR/
 
 ## License
 
-This project is licensed under the MIT License - see the LICENSE file for details.
+This project is licensed under the MIT License - see the [LICENSE.md](LICENSE.md) file for details.
--- internal/models/models.go
+++ internal/models/models.go
@@ -6,3 +6,21 @@ type PriceData struct {
 	Timestamp 	time.Time			`json:"timestamp"`
 	Prices		map[string]Price	`json:"prices"`
 }
+
+type Price struct {
+	Bid		float64	`json:"bid"`
+	Ask		float64	`json:"ask"`
+	Last	float64	`json:"last"`
+}
+
+type OrderbookData struct {
+	Timestamp	time.Time	`json:"timestamp"`
+	BuyOrders	[]Order		`json:"buyOrders"`
+	SellOrders	[]Order		`json:"sellOrders"`
+}
+
+type Order struct {
+	Amount		float64	`json:"amount"`
+	Rate		float64	`json:"rate"`
+	Total		float64	`json:"total"`
+}
